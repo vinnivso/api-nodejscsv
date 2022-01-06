07f92b43e04428f2fa5b5c90d946c5b94b99bb5f
vinnivso <vinnivsob@gmail.com>
2022-01-06 17:23:20 -0300

    Feat: routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import cors from "cors"
 import express from "express"
+import { router } from "./routes"
 
 export const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(router)
 
 const port = process.env.PORT || 3333
 app.listen(port, () => console.log(`Server is running on port ${port}`))
