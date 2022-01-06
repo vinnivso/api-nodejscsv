bb2bb7b64df6999ae6565e4fec5dbed52b450122
vinnivso <vinnivsob@gmail.com>
2022-01-06 17:51:20 -0300

    Feat: multer config

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,13 @@
-import {Router} from "express"
+import {Request, Response, Router} from "express"
+import multer, { Multer } from "multer"
 
 const router:Router = Router()
+const multerConfig:Multer = multer()
+
+router.post("/products", multerConfig.single("file"), (request: Request, response: Response) => {
+  // console.log(request.file) //Desse jeito vai gerar um buffer
+  console.log(request.file?.buffer.toString("utf-8")) //Para visualizarmos se os arquivos estão vindo corretamente do mercadinho.csv
+  return response.send()
+})
 
 export {router}
